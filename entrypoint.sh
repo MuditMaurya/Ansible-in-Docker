ed247867c97bbbebbdea79df3861546db03ffa47
MuditMaurya <muditpmaurya@gmail.com>
2023-05-03 19:19:20 +0530

    add multi-image support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 if [ "$TYPE" = "build" ];then
     echo "BUILD SUCCESS"
-else
+elif [ "$TYPE" = "ubuntu" ];then
+    /etc/init.d/ssh start
+    exec "$@"
+elif [ "$TYPE" = "alpine" ];then
     /usr/sbin/sshd -D
     exec "$@"
 fi
